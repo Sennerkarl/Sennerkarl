40efbf9d2a7e19ea7554ea3cdd3d919a9d108e03
Sennerkarl <senne.alexander@gmail.com>
2020-12-03 22:42:12 +0100

    init

--- web_project/settings.py
+++ web_project/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'postgres', 
-        'USER': 'postgres', 
-        'PASSWORD': 'Computer92!',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
