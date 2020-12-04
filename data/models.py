9bbd233fcc01c8ee9ed92e91b472900fa829960b
Sennerkarl <senne.alexander@gmail.com>
2020-12-04 16:48:17 +0100

    coutnry data 5

--- data/models.py
+++ data/models.py
@@ -17,9 +17,9 @@ class SBPRI(models.Model):
 class WorldBorder(models.Model):
     # Regular Django fields corresponding to the attributes in the
     # world borders shapefile.
-    Country = models.CharField(max_length=50)
-    iso2 = models.CharField('2 Digit ISO', max_length=2)
-    iso3 = models.CharField('3 Digit ISO', max_length=3)
+    Country = models.CharField()
+    iso2 = models.CharField('2 Digit ISO')
+    iso3 = models.CharField('3 Digit ISO')
     un = models.IntegerField('United Nations Code')
     lon = models.FloatField()
     lat = models.FloatField()
